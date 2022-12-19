9c2b9ff71fa1d5fb4bcf84e778dd7684511b75ea
Yaroslav <Valso96@mail.ru>
2022-12-19 20:04:50 +0600

    Сумма элементов в промежутке от m до n

---
+++ task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum(int m, int n)
+{
+    if (n == m)
+        return m;
+    return sum(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(m, n));
