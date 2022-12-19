0784b23e113d5131f3bcd2b893a798339a4690cf
Yaroslav <Valso96@mail.ru>
2022-12-19 20:05:17 +0600

    Функция Аккермана

---
+++ task68/Program.cs
@@ -0,0 +1,19 @@
+﻿int LeviAckerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return LeviAckerman(n - 1, 1);
+    else
+      return LeviAckerman(n - 1, LeviAckerman(n, m - 1));
+}
+
+Console.Clear();
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Значение m = {m};\tзначение n = {n}.");
+Console.WriteLine($"\nA(m,n) = {LeviAckerman(m, n)}\n");
